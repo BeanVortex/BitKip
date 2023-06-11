4d0247bec97470fd5530e9a64b8749f3a434ef2c
DarkDeveloper_Plant <goldeagle00110011@outlook.com>
2023-06-11 13:28:14 +0330

    - added install extension option and logic for that - bug fix on showing messages for update checker

--- src/main/java/ir/darkdeveloper/bitkip/config/AppConfigs.java
+++ src/main/java/ir/darkdeveloper/bitkip/config/AppConfigs.java
@@ -17,7 +17,7 @@ import java.util.*;
 
 public class AppConfigs {
 
-    public static final String VERSION = "0.3.0";
+    public static final String VERSION = "0.4.0";
 
     public static final String dataPath = System.getProperty("user.home")
             + File.separator + "Documents"
--- src/main/java/ir/darkdeveloper/bitkip/utils/MoreUtils.java
+++ src/main/java/ir/darkdeveloper/bitkip/utils/MoreUtils.java
@@ -1,9 +1,12 @@
 package ir.darkdeveloper.bitkip.utils;
 
 import ir.darkdeveloper.bitkip.config.AppConfigs;
+import ir.darkdeveloper.bitkip.task.ExtensionInstallTask;
 import ir.darkdeveloper.bitkip.task.UpdateCheckTask;
+import javafx.application.Platform;
 import org.controlsfx.control.Notifications;
 
+import java.io.IOException;
 import java.util.concurrent.Executors;
 
 import static ir.darkdeveloper.bitkip.config.AppConfigs.log;
@@ -14,21 +17,44 @@ public class MoreUtils {
         var updateChecker = new UpdateCheckTask();
         var executor = Executors.newCachedThreadPool();
         updateChecker.setExecutor(executor);
-        executor.submit(updateChecker);
-
         updateChecker.valueProperty().addListener((obs, old, newVal) -> {
             var version = newVal.version();
             if (!AppConfigs.VERSION.equals(version)) {
                 log.info("New update available: " + version);
                 FxUtils.showUpdateDialog(newVal);
             }
-            else if (showNoUpdatesNotification)
-                Notifications.create()
+        });
+        updateChecker.setOnCancelled(e -> {
+            if (showNoUpdatesNotification)
+                Platform.runLater(() -> Notifications.create()
                         .title("Checked for updates")
                         .text("No updates available")
-                        .showInformation();
+                        .showInformation());
+        });
+        updateChecker.exceptionProperty().addListener((ob, o, ex) -> {
+            if (ex instanceof IOException && showNoUpdatesNotification)
+                Platform.runLater(() -> Notifications.create()
+                        .title("Failed to check updates")
+                        .text("Could not connect to source, check your connection")
+                        .showWarning());
 
         });
+        executor.submit(updateChecker);
+
     }
 
+    public static void downloadExtension() {
+        var exTask = new ExtensionInstallTask();
+        var executor = Executors.newCachedThreadPool();
+        exTask.setExecutor(executor);
+        exTask.exceptionProperty().addListener((ob, o, ex) -> {
+            if (ex instanceof IOException)
+                Platform.runLater(() -> Notifications.create()
+                        .title("Failed to install extension")
+                        .text("Could not connect to source, check your connection")
+                        .showWarning());
+
+        });
+        executor.submit(exTask);
+    }
 }
