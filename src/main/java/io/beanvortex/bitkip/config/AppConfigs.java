3fffce4236b74ac8360c221aae63821ba9818c36
BeanVortex <goldeagle00110011@outlook.com>
2025-01-23 15:08:51 +0330

    - version update - README.md update

--- src/main/java/io/beanvortex/bitkip/config/AppConfigs.java
+++ src/main/java/io/beanvortex/bitkip/config/AppConfigs.java
@@ -17,7 +17,7 @@ import java.util.*;
 
 public class AppConfigs {
 
-    public static final String VERSION = "1.4.9";
+    public static final String VERSION = "1.5.0";
 
     public static final String dataPath = System.getProperty("user.home")
             + File.separator + "Documents"
